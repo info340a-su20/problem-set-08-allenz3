60caad4862a6d0cda95a93e5ae5b2c4a1738480c
allenz3 <allenz3@uw.edu>
2020-08-12 14:35:16 -0700

    problem a finished

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; //import React Component
 
-const EXAMPLE_SENATORS = [  
-  { id: 'C000127',  name: 'Maria Cantwell', state: 'WA',  party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
+const EXAMPLE_SENATORS = [
+  { id: 'C000127', name: 'Maria Cantwell', state: 'WA', party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
   { id: 'M001111', name: 'Patty Murray', state: 'WA', party: 'Democrat', phone: '202-224-2621', twitter: 'PattyMurray' }
 ];
 //<SenatorRow senator = {EXAMPLE_SENATORS[i]}/>
@@ -11,7 +11,7 @@ export class App extends Component {
     return (
       <div className="container">
         <h1>US Senators 2019</h1>
-        <SenatorTable senators = { this.props.senators }/>
+        <SenatorTable senators={this.props.senators} />
       </div>
     );
   }
@@ -19,11 +19,14 @@ export class App extends Component {
 
 export class SenatorTable extends Component {
   render() {
+    const senatorRows = this.props.senators.map((senator) =>
+      <SenatorRow key={senator.id} senator={senator} />
+    )
     return (
       <table className="table table-bordered">
-        <TableHeader cols = {['Name', 'State', 'Phone', 'Twitter']} />
+        <TableHeader cols={['Name', 'State', 'Phone', 'Twitter']} />
         <tbody>
-          <SenatorRow senator = {this.props.senators}/>
+          {senatorRows}
         </tbody>
       </table>
     );
@@ -33,14 +36,14 @@ export class SenatorTable extends Component {
 export class TableHeader extends Component {
   render() {
     const colNames = this.props.cols.map((colName) => {
-      return (<th key={ colName }>
-              { colName }
-              </th>);
+      return (<th key={colName}>
+        {colName}
+      </th>);
     });
 
     return (
       <thead>
-        <tr>{ colNames }</tr>
+        <tr>{colNames}</tr>
       </thead>
     );
   }
@@ -48,26 +51,18 @@ export class TableHeader extends Component {
 
 export class SenatorRow extends Component {
   render() {
-    const renderSenator = (senator, index) => {
-      return (
-          <tr key={ senator.id }>
-            <td>{ senator.name }</td>
-            <td>{ senator.party.substring(0, 1) + " - " + senator.state }</td>
-            <td>
-              <a href={ "tel:+" + senator.phone }>{ senator.phone }</a>
-            </td>
-            <td>
-              <a href={ ("https://twitter.com/" + senator.name).replace(/\s/g, '') }>{ "@" + senator.twitter }</a>
-            </td>
-          </tr>
-        );
-    };
-
-
-    //const row = this.props.senator.map((senator) => { 
-    //});
+    const senator = this.props.senator;
     return (
-        this.props.senator.map(renderSenator)
+      <tr>
+        <td>{senator.name}</td>
+        <td>{senator.party.substring(0, 1) + " - " + senator.state}</td>
+        <td>
+          <a href={"tel:" + senator.phone}>{senator.phone}</a>
+        </td>
+        <td>
+          <a href={("https://twitter.com/" + senator.twitter).replace(/\s/g, '')}>{"@" + senator.twitter}</a>
+        </td>
+      </tr>
     );
   }
 }
